6d9b6c562b3e07818afa4c99315d1a200b4df127
santorinitecnologia <132526613+santorinitecnologia@users.noreply.github.com>
2023-05-03 21:50:29 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass (pré-processador css) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass."
+);
+hoverChangeDescription(
+  ".react",
+  "ReactJS é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web."
+);
+hoverChangeDescription(
+  ".c",
+  "C é uma linguagem de programação compilada de propósito geral."
+);
+hoverChangeDescription(
+  ".python",
+  "Python é uma linguagem de programação de alto nível, dinâmica, interpretada, modular, multiplataforma e orientada a objetos."
+);
+
